406dc8e95b0b135f103ea15740c90996e0162a9b
star2112 <bintangfadel73@gmail.com>
2022-11-15 01:10:31 +0700

    fix

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const DB_URL =
+  process.env.MONGO_URI ||
   "mongodb+srv://admin:admin@mycluster.xntmcj2.mongodb.net/?retryWrites=true&w=majority";
 // const DB_URL = "mongodb://localhost:27017/todo-app";
 
